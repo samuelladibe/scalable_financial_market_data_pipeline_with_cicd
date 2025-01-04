2b08beeaa1bebd38f0e88d4e9100e685242a462a
samuel ladibe <ladibesamuel@gmail.com>
2025-01-04 21:43:53 +0100

    feature: setting django project settings'

--- financial_pipeline/settings.py
+++ financial_pipeline/settings.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+# Define BASE_DIR for the Django project
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv()
 
@@ -9,8 +13,14 @@ DB_USER = os.getenv("DB_USER") # Change this if your username is different
 DB_PASSWORD = os.getenv("DB_PASSWORD") # Replace with your PostgreSQL password
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
     # other apps...
     'django_crontab',
     'app.fetcher',
@@ -19,6 +29,40 @@ INSTALLED_APPS = [
 CRONJOBS = [
     # Schedule, Command to run
     ('0 0 * * *', 'app.fetcher.tasks.run_fetch_and_store'), # Runs daily at midnight
+]
+
+ALLOWED_HOSTS = ['*'] # You can add 'your-server-ip', 'your-domain.com' for deployment
+
+ROOT_URLCONF = 'financial_pipeline.urls'
+
+DEBUG = False
+
+STATIC_URL = '/static/'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Adjust 'templates' folder path as needed
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 DATABASES = {
@@ -30,6 +74,7 @@ DATABASES = {
         'HOST': DB_HOST,  # Or your database server
         'PORT': DB_PORT,  # Default PostgreSQL port
     }
+
 
 # DATABASES = {
 #     'default': {
